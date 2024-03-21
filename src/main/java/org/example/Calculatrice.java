54434be233126b0aa6fbdf8a59a24f6e6a052873
ChaoukiBehaz <behazchaouki@gmail.com>
2024-03-21 09:34:11 +0100

    Initial commit

---
+++ src/main/java/org/example/Calculatrice.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Calculatrice {
+    public int additionner(int a, int b) {
+        result = a + b ;
+        this.getState();
+        return result;
+    }
+    public int getState(){
+        return result;
+    }
+    private int result ;
+}
